bbd510706b0dcad8eb81b4635cc3e0c8e9621248
Alexandre <agil2910@caelum031.(none)>
2012-12-07 11:36:57 -0200

    comitando...hehe

--- src/test/java/br/com/caelum/agiletickets/models/EspetaculoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/EspetaculoTest.java
@@ -3,6 +3,7 @@ package br.com.caelum.agiletickets.models;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.joda.time.LocalDate;
@@ -87,7 +88,7 @@ public class EspetaculoTest {
 	}
 	
 	@Test
-	public void validaCriarSessoes(){
+	public void validaCriarSessoesComMesmaDataInicioEFimPeriodicidadeDiaria(){
 	//LocalDate inicio, LocalDate fim, LocalTime horario, Periodicidade periodicidade) {
 		// ALUNO: Não apague esse metodo. Esse sim será usado no futuro! ;)
 		LocalDate inicio = new LocalDate().withDayOfMonth(1).withMonthOfYear(1).withYear(2012);
@@ -98,8 +99,21 @@ public class EspetaculoTest {
 		
 		Espetaculo show = new Espetaculo();
 		List<Sessao> sessoes = show.criaSessoes(inicio, fim, horario, period);
-			assertFalse(sessoes.size()!=1);
+		assertFalse(sessoes.size()!=1);
 		
+		List<Double> list = new ArrayList<Double>();
+		for (int i=0; i<100; i++){
+			list.add(Double.valueOf(i));
+		}
+		List<Double> list2 = new ArrayList<Double>();
+		for (Double doubles : list) {
+			//System.out.println(integer);
+			if (doubles <10 || doubles >20)
+			list2.add(doubles);
+		}
+		for (Double double1 : list2) {
+			System.out.println(double1);
+		}
 		
 	}
 	
